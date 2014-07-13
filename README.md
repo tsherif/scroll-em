c744686876b7051aadacaf6472db7eacebd4bef7
Tarek Sherif <tsherif@gmail.com>
2014-07-13 09:52:28 -0400

    v0.2. Major optimizations. New API.

--- README.md
+++ README.md
@@ -14,16 +14,108 @@ New scroll animations are added with **ScrollEm.add()**:
   ScrollEm.add(document.getElementById("scrolling-element"), {
     start: 500,
     end: 700,
-    css: {
-      right: {
+    css: [
+      {
+        property: "left",
         start: -500,
         end: 400
       }
-    }
+    ]
   });
 ```
 
-The **start** and **end** options indicate the scroll positions at which to start and end the animation. The **css** option contains the CSS properties to animate and the **start** and **end** values that the CSS property will animate from and to. Optionally, a **units** option can be passed if the animation will be based on something other than *px* (e.g. *em* or *%*).
+The **start** and **end** options indicate the scroll positions at which to start and end the animation. The **css** option contains the CSS properties to animate and the **start** and **end** values that the CSS property will animate from and to. Optionally, a **units** option can be passed if the animation will be based on something other than *px* (e.g. *em* or *%*). 
+
+If no **end** options is given, the property will not be modified by scrolling. This can be useful for setting the initial positions of elements: 
+
+```JavaScript
+  ScrollEm.add(document.getElementById("scrolling-element"), {
+    start: 500,
+    end: 700,
+    css: [
+      {
+        property: "top",
+        start: 200
+      },
+      {
+        property: "left",
+        start: 500
+      }
+    ]
+  });
+```
+
+If possible, it is recommended to use one of the transform shortcuts provided by **ScrollEm** to animate elements. These include: 
+
+ * **translateX**
+ * **translateY**
+ * **translateZ**
+ * **rotate**
+ * **scaleX**
+ * **scaleY**
+ * **scaleZ**
+
+These properties are generally [faster to animate](http://www.html5rocks.com/en/tutorials/speed/high-performance-animations/) than non-transform properties with similar effects, and if the shortcuts are used, **ScrollEm** will attempt to place the element on its own compositing layer.
+
+```JavaScript
+  ScrollEm.add(document.getElementById("scrolling-element"), {
+    start: 500,
+    end: 700,
+    css: [
+      {
+        property: "top",
+        start: 200
+      },
+      {
+        property: "left",
+        start: 500
+      },
+      {
+        property: "translateX",
+        start: 0,
+        end: 100
+      }
+    ]
+  });
+```
+
+If simply appending units isn't appropritate for the property you wish to animate, a **wrapper** function can be provided to convert the calculated value into a valid CSS property:
+
+```JavaScript
+  ScrollEm.add(document.getElementById("scrolling-element"), {
+    start: 500,
+    end: 700,
+    css: [
+      {  
+        property: "transform",
+        start: 0,
+        end: 100,
+        wrapper: function(value) {
+          return "translateX(" + value + "px)"
+        }
+      }
+    ]
+  });
+```
+
+Multiple vendor prefixes for a property can be managed by passing an array of properties to the **properties** option:
+
+```JavaScript
+  ScrollEm.add(document.getElementById("scrolling-element"), {
+    start: 500,
+    end: 700,
+    css: [
+      {  
+        properties: ["transform", "-webkit-transform"],
+        start: 0,
+        end: 100,
+        wrapper: function(value) {
+          return "translateX(" + value + "px)"
+        }
+      }
+    ]
+  });
+```
 
 Instead of setting the **start** and **end** scroll positions directly, it is possible to set a default scroll range (i.e. distance between the scroll start and end positions) using **ScrollEm.setDefaultScrollRange()**, and then only the **start** option is required. Furthermore, for a series of animations, this functionality can be used with ScrollEm's built in position recorder to set new animation scroll positions relative to old ones. The current scroll position being recorded can be moved forwards or backwards using **ScrollEm.forward()** and **ScrollEm.back()**. If the recorded scroll position is used, then both the scrolling **start** and **end** options can be omitted for **ScrollEm.add()**:
 
@@ -33,23 +125,25 @@ Instead of setting the **start** and **end** scroll positions directly, it is po
   ScrollEm.forward(300);
 
   ScrollEm.add(document.getElementById("scrolling-element1"), {
-    css: {
-      right: {
+    css: [
+      {
+        property: "right",
         start: -500,
         end: 400
       }
-    }
+    ]
   });
 
   ScrollEm.forward(300);
 
   ScrollEm.add(document.getElementById("scrolling-element2"), {
-    css: {
-      left: {
+    css: [
+      {
+        property: "left",
         start: -500,
         end: 400
       }
-    }
+    ]
   });
 ```
 
@@ -69,12 +163,13 @@ Strings can be used to dynamically set CSS values based on page dimensions at th
 
 ```JavaScript
   ScrollEm.add(document.getElementById("scrolling-element"), {
-    css: {
-      left: {
+    css: [
+      {
+        property: "left",
         start: "-WINDOW_WIDTH",
         end: "MARGIN_LEFT + CONTAINER_WIDTH - 100"
       }
-    }
+    ]
   });
 ```
 
--- src/scroll-em.js
+++ src/scroll-em.js
@@ -24,11 +24,63 @@
 (function() {
   "use strict";
 
-  var WINDOW_WIDTH, WINDOW_HEIGHT;
-  var CONTAINER_WIDTH, MARGIN_LEFT, MARGIN_RIGHT;
+  var DIMENSIONS = {
+    WINDOW_WIDTH: 0,
+    WINDOW_HEIGHT: 0,
+    CONTAINER_WIDTH: 0,
+    MARGIN_LEFT: 0,
+    MARGIN_RIGHT: 0
+  };
+
+  var DIMENSION_FUNCTIONS = {
+    "WINDOW_WIDTH": function() {
+      return DIMENSIONS.WINDOW_WIDTH;
+    },
+    "-WINDOW_WIDTH": function() {
+      return -DIMENSIONS.WINDOW_WIDTH;
+    },
+    "WINDOW_HEIGHT": function() {
+      return DIMENSIONS.WINDOW_HEIGHT;
+    },
+    "-WINDOW_HEIGHT": function() {
+      return -DIMENSIONS.WINDOW_HEIGHT;
+    },
+    "CONTAINER_WIDTH": function() {
+      return DIMENSIONS.CONTAINER_WIDTH;
+    },
+    "-CONTAINER_WIDTH": function() {
+      return -DIMENSIONS.CONTAINER_WIDTH;
+    },
+    "MARGIN_LEFT": function() {
+      return DIMENSIONS.MARGIN_LEFT;
+    },
+    "-MARGIN_LEFT": function() {
+      return -DIMENSIONS.MARGIN_LEFT;
+    },
+    "MARGIN_RIGHT": function() {
+      return DIMENSIONS.MARGIN_RIGHT;
+    },
+    "-MARGIN_RIGHT": function() {
+      return -DIMENSIONS.MARGIN_RIGHT;
+    }
+  };
+
+  var DIMENSION_REGEX = new RegExp(
+    "(" + Object.keys(DIMENSIONS).join("|") + ")",
+    "g"
+  );
 
   function parseValue(value) {
-    return typeof value === "string" ? eval(value) : value;
+    if (typeof value === "string") {
+      if (typeof DIMENSION_FUNCTIONS[value] !== "function") {
+        DIMENSION_FUNCTIONS[value] = new Function(
+          "return " + value.replace(DIMENSION_REGEX, "ScrollEm.DIMENSIONS.$1") + ";"
+        );
+      }
+      value = DIMENSION_FUNCTIONS[value]();
+    }
+
+    return  value;
   }
 
   // Hide scope from eval
@@ -50,9 +102,52 @@
     var default_css_body_position = BODY.style.position;
     var default_css_body_height = BODY.style.height;
 
+    var do_update = false;
+    var TRANSFORM_PROPERTIES = ["-webkit-transform", "-ms-transform", "transform"];
+    var transform_counter = 0;
+
+    var TRANSFORMS = {
+      "translateX": {
+        wrapper: function(value) {
+          return "translateX(" + value + "px)";
+        }
+      },
+      "translateY": {
+        wrapper: function(value) {
+          return "translateY(" + value + "px)";
+        }
+      },
+      "translateZ": {
+        wrapper: function(value) {
+          return "translateZ(" + value + "px)";
+        }
+      },
+      "rotate": {
+        wrapper: function(value) {
+          return "rotate(" + value + "deg)";
+        }
+      },
+      "scaleX": {
+        wrapper: function(value) {
+          return "scaleX(" + value + ")";
+        }
+      },
+      "scaleY": {
+        wrapper: function(value) {
+          return "scaleY(" + value + ")";
+        }
+      },
+      "scaleZ": {
+        wrapper: function(value) {
+          return "scaleZ(" + value + ")";
+        }
+      },
+    };
+
     resetDimensions();
 
     window.ScrollEm = {
+      DIMENSIONS: DIMENSIONS,
       add: function(element, options) {
         var scroll_element = { element: element };
 
@@ -60,18 +155,46 @@
         scroll_element.end = options.hasOwnProperty("end") ? options.end : scroll_element.start + scroll_range;
 
 
-        scroll_element.css = options.css || {};
+        scroll_element.css = {};
         
-        Object.keys(scroll_element.css).forEach(function(property) {
-          var start = scroll_element.css[property].start = scroll_element.css[property].start === undefined ? 0 : scroll_element.css[property].start;
-          var units = scroll_element.css[property].units = scroll_element.css[property].units === undefined ? "px" : scroll_element.css[property].units;
-          scroll_element.css[property].end = scroll_element.css[property].end === undefined ? start : scroll_element.css[property].end;
+        options.css.forEach(function(property_options) {
+          var property = property_options.property || property_options.properties;
+          var property_list;
 
-          scroll_element.css[property].default = element.style[property];
-
-          if (window.innerWidth >= min_width) {
-            element.style[property] = parseValue(start) + units;
+          if (TRANSFORMS[property]) {
+            property_list = ["transform-" + transform_counter++];
+            property_options.wrapper = property_options.wrapper || TRANSFORMS[property].wrapper;
+            property_options.transform = true;
+          } else {
+            property_list = Array.isArray(property) ? property : [property];
           }
+
+          property_list.forEach(function(property) {
+            var property_settings = { transform: property_options.transform };
+
+            var start = property_settings.start = property_options.start === undefined ? 0 : property_options.start;
+            var units = property_settings.units = property_options.units === undefined ? "px" : property_options.units;
+            var wrapper = property_settings.wrapper = property_options.wrapper;
+            property_settings.end = property_options.end === undefined ? start : property_options.end;
+
+            scroll_element.css[property] = property_settings;
+
+            if (property_settings.transform) {
+              return;
+            }
+
+            property_settings.default = element.style[property];
+
+            if (window.innerWidth >= min_width) {
+              if (typeof wrapper === "function") {
+                element.style[property] = property_settings.current_value = wrapper(parseValue(start));
+              } else {
+                element.style[property] = property_settings.current_value = parseValue(start) + units;
+              }
+            }
+
+          });
+        
         });
 
         scroll_elements.push(scroll_element);
@@ -152,42 +275,88 @@
     };
 
 
-    window.addEventListener("scroll", update);
+    window.addEventListener("scroll", function() {
+      do_update = true;
+    });
+
+    window.requestAnimationFrame(update);
 
     window.addEventListener("resize", resetDimensions);
 
     function update() {
+      window.requestAnimationFrame(update);
+
+      if (!do_update) return;
+
       var position = window.pageYOffset;
+      do_update = false;
+
       scroll_elements.forEach(function(scroll_element) {
         var proportion = (position - scroll_element.start) / (scroll_element.end - scroll_element.start);
         proportion = Math.max(0, Math.min(proportion, 1));
+        var transform_list = [];
 
         Object.keys(scroll_element.css).forEach(function(property) {
-          if (window.innerWidth < min_width) {
-            scroll_element.element.style[property] = scroll_element.css[property].default;
+          var property_settings = scroll_element.css[property];
+
+          if (DIMENSIONS.WINDOW_WIDTH < min_width) {
+            if (!property_settings.transform && property_settings.current_value !== property_settings.default) {
+              scroll_element.element.style[property] = property_settings.current_value = property_settings.default;
+            }
             return;
           }
 
-          var start = parseValue(scroll_element.css[property].start);
-          var end = parseValue(scroll_element.css[property].end);
-          var units = scroll_element.css[property].units;
+
+          var start = parseValue(property_settings.start);
+          var end = parseValue(property_settings.end);
+          var units = property_settings.units;
+          var wrapper = property_settings.wrapper;
 
           var value = start + proportion * (end - start);
-          scroll_element.element.style[property] = value + units;
+
+          if (typeof wrapper === "function") {
+            value = wrapper(value);
+          } else {
+            value = value + units;
+          }
+
+          if (property_settings.transform) {
+            transform_list.push(value);
+          } else if (property_settings.current_value !== value) {
+            scroll_element.element.style[property] = property_settings.current_value = value;
+          }
         });
+  
+        
+        if (DIMENSIONS.WINDOW_WIDTH < min_width) {
+          TRANSFORM_PROPERTIES.forEach(function(property) {
+            scroll_element.element.style[property] = "none";
+          });
+        } else if (transform_list.length > 0) {
+          TRANSFORM_PROPERTIES.forEach(function(property) {
+            var transform = transform_list.join(" ");
+
+            if (!transform.match(/translateZ|translate3D/)) {
+              transform += " translateZ(0)";
+            }
+
+            scroll_element.element.style[property] = transform;
+          });
+        }
+  
       });
 
     }
 
     function resetDimensions() {
-      WINDOW_WIDTH = CONTAINER_WIDTH = window.innerWidth;
-      WINDOW_HEIGHT = window.innerHeight;
-      MARGIN_LEFT = MARGIN_RIGHT = 0;
+      DIMENSIONS.WINDOW_WIDTH = DIMENSIONS.CONTAINER_WIDTH = window.innerWidth;
+      DIMENSIONS.WINDOW_HEIGHT = window.innerHeight;
+      DIMENSIONS.MARGIN_LEFT = DIMENSIONS.MARGIN_RIGHT = 0;
 
       if (container) {
-        CONTAINER_WIDTH = container.offsetWidth;
-        MARGIN_LEFT = getLeftOffset(container);
-        MARGIN_RIGHT = WINDOW_WIDTH - (MARGIN_LEFT + CONTAINER_WIDTH);
+        DIMENSIONS.CONTAINER_WIDTH = container.offsetWidth;
+        DIMENSIONS.MARGIN_LEFT = getLeftOffset(container);
+        DIMENSIONS.MARGIN_RIGHT = DIMENSIONS.WINDOW_WIDTH - (DIMENSIONS.MARGIN_LEFT + DIMENSIONS.CONTAINER_WIDTH);
       }
 
       if (window.innerWidth < min_width) {
@@ -214,7 +383,7 @@
         });
       }
 
-      update();
+      do_update = true;
     }
 
     function getLeftOffset(element) {
